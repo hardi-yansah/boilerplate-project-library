9bddd2e63130ce1a13859cb03960606fce985b63
hardi-yansah <hardi.yansah97@gmail.com>
2025-05-26 01:31:02 +0700

    fix: func no2

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,10 +14,6 @@ const server = require("../server");
 chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
-  /*
-   * ----[EXAMPLE TEST]----
-   * Each test should completely test the response of the API end-point including response status code!
-   */
   test("#example Test GET /api/books", function (done) {
     chai
       .request(server)
@@ -25,21 +21,23 @@ suite("Functional Tests", function () {
       .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.isArray(res.body, "response should be an array");
-        assert.property(
-          res.body[0],
-          "commentcount",
-          "Books in array should contain commentcount"
-        );
-        assert.property(
-          res.body[0],
-          "title",
-          "Books in array should contain title"
-        );
-        assert.property(
-          res.body[0],
-          "_id",
-          "Books in array should contain _id"
-        );
+        if (res.body.length > 0) {
+          assert.property(
+            res.body[0],
+            "commentcount",
+            "Books in array should contain commentcount"
+          );
+          assert.property(
+            res.body[0],
+            "title",
+            "Books in array should contain title"
+          );
+          assert.property(
+            res.body[0],
+            "_id",
+            "Books in array should contain _id"
+          );
+        }
         done();
       });
   });
